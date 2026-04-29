fdda0e0048ea22b70d9dbfc092765412690dff4b
Laurent <turpin.laurent@gmail.com>
2026-04-29 13:00:35 +0400

    force dynamic animaux

--- src/app/animaux/page.tsx
+++ src/app/animaux/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 import React, { useState, useMemo, useEffect } from 'react'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
